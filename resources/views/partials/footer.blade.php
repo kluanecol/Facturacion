ee628beaa77ae124e6ffae5d0f0d77ce63b8a522
Jason Rizo <jasonrizo84@gmail.com>
2022-09-14 21:47:37 -0500

    Footer fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="page-footer bg-secondary text-center">
+<div class="page-footer bg-dark text-center py-2" style="bottom: 0; position: fixed; width: 100%;">
     <div class="page-footer-inner"> <script>document.write(new Date().getFullYear())</script> &copy;
     	Desarrollado con <i class="fa fa-heart" aria-hidden="true" style="color: blue;"></i> por el área de tecnología
         <a target="_blank" href="javascript:;">{{ config('app.author') }}</a> - Kluane Colombia &nbsp;|&nbsp;
