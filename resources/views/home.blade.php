7f578d153f9ee0f09ab27a9508a35d3d3f2201e3
Jason Rizo <jasonrizo84@gmail.com>
2022-10-21 21:56:45 -0500

    test lang

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Panel administrativo')
 
 @section('card-icon')<i class="icofont-document-folder"></i>@endsection
-@section('card-title', strtoupper(trans('messages\general.rhombFacturacion')))
+@section('card-title', strtoupper(trans('general.rhombFacturacion')))
 
 @section('content')
 
