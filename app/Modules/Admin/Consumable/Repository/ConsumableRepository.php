bb15fd8ee7dee8b52305a159bcf4190d2993d119
Jason Rizo <jasonrizo84@gmail.com>
2022-10-28 13:04:39 -0500

    group

--- app/Modules/Admin/Consumable/Repository/ConsumableRepository.php
+++ app/Modules/Admin/Consumable/Repository/ConsumableRepository.php
@@ -28,8 +28,8 @@ class ConsumableRepository implements ConsumableInterface{
             $q->where('nombre', 'like', '%' .$string. '%')->orWhere('nombre_ingles', 'like', '%' .$string. '%');
         })
         ->orderBy('nombre')
-        ->take(100)
         ->groupBy('id','id_grupo')
+        ->take(100)
         ->get();
     }
 
