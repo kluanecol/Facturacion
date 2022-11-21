4828a3ac264f7f1d89f8543481dec01cfda03071
Jason Rizo <jasonrizo84@gmail.com>
2022-11-20 19:45:12 -0500

    Lógica botón cambiar estado

--- resources/views/sections/parametrics/components/table-options.blade.php
+++ resources/views/sections/parametrics/components/table-options.blade.php
@@ -1,7 +1,10 @@
 
 <a  class="btn  btn-warning edit-parametric" data-id="{{$parametric->id}}" target="_blank"><i class="icofont-pencil-alt-1"></i></a>
-<span class="tool" data-tip="{{trans('buttons.cambiarEstado')}}"tabindex="1">
-    <a  class="btn  btn-outline-success disable-parametric" data-id="{{$parametric->id}}" target="_blank" > <i class="icofont-power"></i></a>
-</span>
+
+@if ( sizeof($parametric->json_countries) == 1)
+    <span class="tool" data-tip="{{trans('buttons.cambiarEstado')}}"tabindex="1">
+        <a  class="btn  btn-outline-success disable-parametric" data-id="{{$parametric->id}}" target="_blank" > <i class="icofont-power"></i></a>
+    </span>
+@endif
 
 
