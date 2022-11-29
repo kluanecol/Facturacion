1207842d7d07f411595aedf784cf78d1c92cd067
Jason Rizo <jasonrizo84@gmail.com>
2022-11-29 10:43:46 -0500

    upper case fix

--- app/Modules/Invoicing/Invoice/models/Invoice.php
+++ app/Modules/Invoicing/Invoice/models/Invoice.php
@@ -4,7 +4,7 @@ namespace App\Modules\Invoicing\Invoice\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Modules\Invoicing\Contract\Models\contract;
+use App\Modules\Invoicing\Contract\Models\Contract;
 
 class Invoice extends Model
 {
